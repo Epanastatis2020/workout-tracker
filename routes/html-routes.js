19cc8046cf0e17a9de2123912cedc16b2f5b27a9
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-12-10 18:36:54 +1100

    added routes in html-routes for index, exercise and stats pages

---
+++ routes/html-routes.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+
+// Requiring our models
+const db = require("../models");
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app){
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });      
+  
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });  
+    
+    app.get("*", function (req, res) {
+         res.sendFile(path.join(__dirname, "../public/index.html"));
+    }); 
+};
