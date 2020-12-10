c5d781d49551a52045b0b9cefdec8d22ee61d216
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-12-10 18:35:17 +1100

    added routes in api-routes for GET, POST and PUT

---
+++ routes/api-routes.js
@@ -0,0 +1,57 @@
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+const mongojs = require("mongojs");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all workouts
+    app.get("/api/workouts", function(req, res) {
+        db.Workout.find({})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+  // GET route for getting workouts in the last 7 days
+    app.get("/api/workouts/range", function(req, res) {
+        db.Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });    
+
+    //POST route for creating a new workout
+    app.post("/api/workouts", ({body}, res) => {
+        db.Workout.create(body)
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    });
+
+    //PUT route for updating an existing workout
+    app.put("/api/workouts/:id", (req, res) => {      
+        // db.Workout.findByIdAndUpdate({"_id": mongojs.ObjectId(req.params.id)},{$set:{"exercises":req.body}}, (err, data) => {     
+        db.Workout.findByIdAndUpdate({"_id": mongojs.ObjectId(req.params.id)},{$push: {"exercises":req.body}}, {new: true}, (err, data) => {                 
+            if (err) {
+              console.log(err);
+            } else {
+              res.json(data);
+            }
+        }); 
+    });  
+
+};
